1ce4630e01c206c818fde7927d9186586f59715b
Ricardo Aravena <raravena@branch.io>
2017-10-14 20:30:27 -0700

    Skeleton for ssh session

--- cmd/root.go
+++ cmd/root.go
@@ -19,23 +19,24 @@ import (
 	"os"
 
 	homedir "github.com/mitchellh/go-homedir"
+	"github.com/raravena80/sshrunner/exec"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
-var cfgFile string
+var (
+	cfgFile, command string
+	machines         []string
+)
 
 // RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
 	Use:   "sshrunner",
 	Short: "Sshrunner runs ssh commands across multiple servers",
 	Long:  `Sshrunner runs ssh commands across multiple servers`,
-
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+	// Bare app run
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Hello")
+		exec.Run(machines, command)
 	},
 }
 
@@ -59,6 +60,8 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	RootCmd.Flags().StringArrayVarP(&machines, "machines", "m", []string{}, "Hosts to run command on")
+	RootCmd.Flags().StringVarP(&command, "command", "c", "", "Commond to run")
 }
 
 // initConfig reads in config file and ENV variables if set.
---
+++ exec/run.go
@@ -0,0 +1,94 @@
+// Copyright © 2017 Ricardo Aravena <raravena80@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package exec
+
+import (
+	"bytes"
+	"fmt"
+	"golang.org/x/crypto/ssh"
+	"io/ioutil"
+	"os"
+	"time"
+)
+
+type SignerContainer struct {
+	signers []ssh.Signer
+}
+
+func makeSigner(keyname string) (signer ssh.Signer, err error) {
+	fp, err := os.Open(keyname)
+	if err != nil {
+		return
+	}
+	defer fp.Close()
+
+	buf, _ := ioutil.ReadAll(fp)
+	signer, _ = ssh.ParsePrivateKey(buf)
+	return
+}
+
+func makeKeyring() ssh.AuthMethod {
+	signers := []ssh.Signer{}
+	keys := []string{os.Getenv("HOME") + "/.ssh/id_rsa", os.Getenv("HOME") + "/.ssh/id_dsa"}
+	for _, keyname := range keys {
+		signer, err := makeSigner(keyname)
+		if err == nil {
+			signers = append(signers, signer)
+		}
+	}
+	return ssh.PublicKeys(signers...)
+}
+
+func executeCmd(cmd, hostname string, config *ssh.ClientConfig) string {
+	conn, _ := ssh.Dial("tcp", hostname+":22", config)
+	session, _ := conn.NewSession()
+	defer session.Close()
+
+	var stdoutBuf bytes.Buffer
+	session.Stdout = &stdoutBuf
+	session.Run(cmd)
+
+	return hostname + ": " + stdoutBuf.String()
+}
+
+// Run the ssh command
+func Run(machines []string, cmd string) {
+	// in 5 seconds the message will come to timeout channel
+	timeout := time.After(5 * time.Second)
+	results := make(chan string, 10)
+
+	config := &ssh.ClientConfig{
+		//User: os.Getenv("LOGNAME"),
+		User: "ubuntu",
+		Auth: []ssh.AuthMethod{makeKeyring()},
+	}
+
+	for _, m := range machines {
+		go func(hostname string) {
+			results <- executeCmd(cmd, hostname, config)
+			// we’ll write results into the buffered channel of strings
+		}(m)
+	}
+
+	for i := 0; i < len(machines); i++ {
+		select {
+		case res := <-results:
+			fmt.Print(res)
+		case <-timeout:
+			fmt.Println("Timed out!")
+			return
+		}
+	}
+}
